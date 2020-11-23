afc7be5959c5863622d3bfec487a1748e8714537
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-23 19:48:18 +0000

    our own assertion function and prints out colourful message

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("dog", "dog");
+assertEqual(2,4);
