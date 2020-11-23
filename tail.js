1521c70834c0e82373801014217788010407dd67
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-23 21:02:02 +0000

    function to return tail of array

---
+++ tail.js
@@ -0,0 +1,32 @@
+// assertEqual function to check results
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// tail function to return array without the first element
+const tail = function(array) {
+  //console.log(array.slice(1));
+  return array.slice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+let result = tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!l have 3 elements!inal array should still have 3 elements!l have 3 elements!!inal array should still have 3 elements!l have 3 elements!
+// Test Case: Check the returned array elements
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// Test an array with only one element to yield an empty array
+const test1 = ['hello'];
+let result1 = tail(test1);
+console.log(result1);
+
+// Test an empty array to yield an empty array
+const test2 = [];
+let result2 = tail(test2);
+console.log(result2);
