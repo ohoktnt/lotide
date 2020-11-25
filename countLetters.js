a57f5d4c29bd08cfdd074a1c9671a947f211abd8
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-25 18:32:23 +0000

    function to count characters in a string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+// Implement countLetters in Lotide
+
+// Assertion function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// countLetters Function
+const countLetters = function(str) {
+  const result = {};
+  str = str.split(' ').join('');
+  for (const letter of str) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+
+// Testing
+console.log(countLetters("tammy is cool"));
+assertEqual(countLetters('LHL')["L"], 2);
