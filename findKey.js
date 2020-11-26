8c39348c93b858fff1d4494f8e2191739b54a2b8
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-26 20:23:01 +0000

    higher order function to find key

---
+++ findKey.js
@@ -0,0 +1,56 @@
+// Implement findKey Lotide Challenge
+// function which takes in an object and a call back
+// it should scan the object and return the first key for which the
+// callback returns a truthy value, if no key is found, then
+// it should return undefined
+
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FINDKEY IMPLEMENTATION
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+// Testing code
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0); // => "Akaleri"
+
+assertEqual(result, 'noma');
+assertEqual(result1, "Akaleri");
+assertEqual(result2, undefined);
