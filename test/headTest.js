6b9d098be28ac192a813a981f14c1ec86d5b1f12
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-30 18:59:32 +0000

    updated test to use Chai assertion

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,28 @@
 // test/head.js
 
-// assertion function
-const assertEqual = require('../assertEqual');
+// chai assertion
+const assert = require('chai').assert;
 
 // head function
 const head = require('../head');
 
-// Test Code
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 0);
-assertEqual(head([1]), 1);
+describe("#head", () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5'],), '5');
+  });
+  it('returns 5 for [5,6,7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it('returns 1 for [1]', () => {
+    assert.strictEqual(head([1]), 1);
+  });
+});
