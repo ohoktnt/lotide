715a8eb7055008747fa8e1e889393ac83d2aa036
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-30 18:27:48 +0000

    removed test codes and added export function

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,6 @@
 // Impletment eqArrays
 // a function that can compare two arrays for a perfect match
 
-// assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⚠️ ⚠️ ⚠️  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // eqArrays function
 const eqArrays = function(arr1, arr2) {
   let result = '';
@@ -34,16 +25,4 @@ const eqArrays = function(arr1, arr2) {
   return result;
 };
 
-// testing
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-// testing nested arrays
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-assertEqual(eqArrays([1,[2],[[[3]]]],[1,[2],[[[3]]]]), true);
-assertEqual(eqArrays([[[[[1]]]]], [1]), false);
+module.exports = eqArrays;
