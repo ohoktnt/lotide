cc282cce4cee3e3e02a150898f19fc13a7e7af39
Tammy Tran <tammy.n.tran@gmail.com>
2020-11-30 18:34:28 +0000

    test codes for assertArraysEqual.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+// test/assertArraysEqualTest.js
+
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Test codes
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([2,3,4], [2,6,4]);
